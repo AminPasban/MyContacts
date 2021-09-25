94fc1c25887e636bf39c838cbcb34d4be1551ddb
amin pasban <amin.pasban.talesh@gmail.com>
2021-09-25 22:53:47 +0330

    project interface added: contact's operations rules added to project.

---
+++ Repository/IMyContactsOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContacts
+{
+    interface IMyContactsOperations
+    {
+        DataTable SelectAll();
+
+        DataTable Select(int id);
+
+        bool Insert(string name, String family, String phone, String email, string address);
+
+        bool Update(int id, string name, String family, String phone, String email, string address);
+
+        bool Delete(int id);
+    }
+}
