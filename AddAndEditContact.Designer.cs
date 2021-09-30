872ef539ae6e1f5dcb4d2fa9241738ae67f59e70
amin pasban <amin.pasban.talesh@gmail.com>
2021-09-30 22:45:37 +0330

    "Add and edit contacts form" added to project.

---
+++ AddAndEditContact.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace MyContacts
+{
+    partial class AddAndEditContact_FORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Name_INP = new System.Windows.Forms.TextBox();
+            this.Family_INP = new System.Windows.Forms.TextBox();
+            this.Phone_INP = new System.Windows.Forms.TextBox();
+            this.Email_INP = new System.Windows.Forms.TextBox();
+            this.Address_INP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddAndEdit_BTN = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.Name_INP);
+            this.groupBox1.Controls.Add(this.Family_INP);
+            this.groupBox1.Controls.Add(this.Phone_INP);
+            this.groupBox1.Controls.Add(this.Email_INP);
+            this.groupBox1.Controls.Add(this.Address_INP);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 341);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Contact informatin";
+            // 
+            // Name_INP
+            // 
+            this.Name_INP.Location = new System.Drawing.Point(15, 71);
+            this.Name_INP.Name = "Name_INP";
+            this.Name_INP.Size = new System.Drawing.Size(196, 27);
+            this.Name_INP.TabIndex = 1;
+            // 
+            // Family_INP
+            // 
+            this.Family_INP.Location = new System.Drawing.Point(248, 71);
+            this.Family_INP.Name = "Family_INP";
+            this.Family_INP.Size = new System.Drawing.Size(196, 27);
+            this.Family_INP.TabIndex = 1;
+            // 
+            // Phone_INP
+            // 
+            this.Phone_INP.Location = new System.Drawing.Point(15, 156);
+            this.Phone_INP.Name = "Phone_INP";
+            this.Phone_INP.Size = new System.Drawing.Size(196, 27);
+            this.Phone_INP.TabIndex = 1;
+            // 
+            // Email_INP
+            // 
+            this.Email_INP.Location = new System.Drawing.Point(248, 156);
+            this.Email_INP.Name = "Email_INP";
+            this.Email_INP.Size = new System.Drawing.Size(196, 27);
+            this.Email_INP.TabIndex = 1;
+            // 
+            // Address_INP
+            // 
+            this.Address_INP.Location = new System.Drawing.Point(15, 241);
+            this.Address_INP.Multiline = true;
+            this.Address_INP.Name = "Address_INP";
+            this.Address_INP.Size = new System.Drawing.Size(429, 84);
+            this.Address_INP.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(245, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Family";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(245, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(12, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // AddAndEdit_BTN
+            // 
+            this.AddAndEdit_BTN.BackColor = System.Drawing.Color.SeaGreen;
+            this.AddAndEdit_BTN.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.AddAndEdit_BTN.ForeColor = System.Drawing.Color.White;
+            this.AddAndEdit_BTN.Location = new System.Drawing.Point(144, 373);
+            this.AddAndEdit_BTN.Name = "AddAndEdit_BTN";
+            this.AddAndEdit_BTN.Size = new System.Drawing.Size(196, 47);
+            this.AddAndEdit_BTN.TabIndex = 1;
+            this.AddAndEdit_BTN.Text = "Add contact";
+            this.AddAndEdit_BTN.UseVisualStyleBackColor = false;
+            this.AddAndEdit_BTN.Click += new System.EventHandler(this.AddAndEdit_BTN_Click);
+            // 
+            // AddAndEditContact_FORM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 441);
+            this.Controls.Add(this.AddAndEdit_BTN);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AddAndEditContact_FORM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.AddAndEditContact_FORM_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Name_INP;
+        private System.Windows.Forms.TextBox Family_INP;
+        private System.Windows.Forms.TextBox Email_INP;
+        private System.Windows.Forms.TextBox Address_INP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Phone_INP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button AddAndEdit_BTN;
+    }
+}
---
+++ AddAndEditContact.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyContacts
+{
+    public partial class AddAndEditContact_FORM : Form
+    {
+        IMyContactsOperations contactsOperations;
+
+        public int UpdatingContactId = 0;
+
+        public AddAndEditContact_FORM()
+        {
+            InitializeComponent();
+            contactsOperations = new MyContactOperations();
+        }
+
+        private void AddAndEditContact_FORM_Load(object sender, EventArgs e)
+        {
+            if (UpdatingContactId != 0)
+            {
+                DataTable updatingContact = contactsOperations.Select(UpdatingContactId);
+                Name_INP.Text = Convert.ToString(updatingContact.Rows[0][1]);
+                Family_INP.Text = Convert.ToString(updatingContact.Rows[0][2]);
+                Phone_INP.Text = Convert.ToString(updatingContact.Rows[0][3]);
+                Email_INP.Text = Convert.ToString(updatingContact.Rows[0][4]);
+                Address_INP.Text = Convert.ToString(updatingContact.Rows[0][5]);
+
+                AddAndEdit_BTN.Text = "Save Changes";
+            }
+        }
+
+        private bool validation()
+        {
+            if (Name_INP.Text == "")
+            {
+                MessageBox.Show("Please enter contact name!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (Family_INP.Text == "")
+            {
+                MessageBox.Show("Please enter contact family!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (Phone_INP.Text == "")
+            {
+                MessageBox.Show("Please enter contact phone!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private void AddAndEdit_BTN_Click(object sender, EventArgs e)
+        {
+            if (validation())
+            {
+                bool isSuccess = UpdatingContactId != 0 
+                    ?
+                    contactsOperations.Update(UpdatingContactId, Name_INP.Text, Family_INP.Text, Phone_INP.Text, Email_INP.Text, Address_INP.Text) 
+                    :
+                    contactsOperations.Insert(Name_INP.Text, Family_INP.Text, Phone_INP.Text, Email_INP.Text, Address_INP.Text);
+
+                if (isSuccess && UpdatingContactId != 0)
+                {
+                    MessageBox.Show("The changes was saved successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    DialogResult = DialogResult.OK;
+                }
+                else if (isSuccess && UpdatingContactId == 0)
+                {
+                    MessageBox.Show("The contact was saved successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("The operation failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
