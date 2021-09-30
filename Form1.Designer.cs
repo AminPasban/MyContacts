a1c7256c96812d57f974fe4a69b68e6c40cdd913
amin pasban <amin.pasban.talesh@gmail.com>
2021-09-30 22:52:40 +0330

    Add contact btn, Remove contact btn, Edit contact btn, Search contact and those's events added to Form1.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,12 @@ namespace MyContacts
             this.ContactPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ContactEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ContactAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddContact_BTN = new System.Windows.Forms.Button();
+            this.RemoveContact_BTN = new System.Windows.Forms.Button();
+            this.EditContact_BTN = new System.Windows.Forms.Button();
+            this.Search_INP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Refresh_BTN = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Contacts_GV)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,6 +50,7 @@ namespace MyContacts
             this.Contacts_GV.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
             this.Contacts_GV.AllowUserToAddRows = false;
             this.Contacts_GV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Contacts_GV.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Contacts_GV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Contacts_GV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ContactId,
@@ -52,11 +59,11 @@ namespace MyContacts
             this.ContactPhone,
             this.ContactEmail,
             this.ContactAddress});
-            this.Contacts_GV.Location = new System.Drawing.Point(13, 152);
+            this.Contacts_GV.Location = new System.Drawing.Point(12, 134);
             this.Contacts_GV.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Contacts_GV.Name = "Contacts_GV";
             this.Contacts_GV.ReadOnly = true;
-            this.Contacts_GV.Size = new System.Drawing.Size(758, 395);
+            this.Contacts_GV.Size = new System.Drawing.Size(760, 397);
             this.Contacts_GV.TabIndex = 0;
             // 
             // ContactId
@@ -65,6 +72,7 @@ namespace MyContacts
             this.ContactId.HeaderText = "Id";
             this.ContactId.Name = "ContactId";
             this.ContactId.ReadOnly = true;
+            this.ContactId.Visible = false;
             // 
             // ContactName
             // 
@@ -101,11 +109,80 @@ namespace MyContacts
             this.ContactAddress.Name = "ContactAddress";
             this.ContactAddress.ReadOnly = true;
             // 
+            // AddContact_BTN
+            // 
+            this.AddContact_BTN.BackColor = System.Drawing.Color.SeaGreen;
+            this.AddContact_BTN.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.AddContact_BTN.ForeColor = System.Drawing.Color.White;
+            this.AddContact_BTN.Location = new System.Drawing.Point(12, 12);
+            this.AddContact_BTN.Name = "AddContact_BTN";
+            this.AddContact_BTN.Size = new System.Drawing.Size(150, 37);
+            this.AddContact_BTN.TabIndex = 1;
+            this.AddContact_BTN.Text = "Add contact";
+            this.AddContact_BTN.UseVisualStyleBackColor = false;
+            this.AddContact_BTN.Click += new System.EventHandler(this.AddContact_BTN_Click);
+            // 
+            // RemoveContact_BTN
+            // 
+            this.RemoveContact_BTN.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.RemoveContact_BTN.Location = new System.Drawing.Point(544, 96);
+            this.RemoveContact_BTN.Name = "RemoveContact_BTN";
+            this.RemoveContact_BTN.Size = new System.Drawing.Size(72, 30);
+            this.RemoveContact_BTN.TabIndex = 4;
+            this.RemoveContact_BTN.Text = "Remove";
+            this.RemoveContact_BTN.UseVisualStyleBackColor = true;
+            this.RemoveContact_BTN.Click += new System.EventHandler(this.RemoveContact_BTN_Click);
+            // 
+            // EditContact_BTN
+            // 
+            this.EditContact_BTN.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.EditContact_BTN.Location = new System.Drawing.Point(622, 96);
+            this.EditContact_BTN.Name = "EditContact_BTN";
+            this.EditContact_BTN.Size = new System.Drawing.Size(72, 30);
+            this.EditContact_BTN.TabIndex = 4;
+            this.EditContact_BTN.Text = "Edit";
+            this.EditContact_BTN.UseVisualStyleBackColor = true;
+            this.EditContact_BTN.Click += new System.EventHandler(this.EditContact_BTN_Click);
+            // 
+            // Search_INP
+            // 
+            this.Search_INP.Location = new System.Drawing.Point(12, 100);
+            this.Search_INP.Name = "Search_INP";
+            this.Search_INP.Size = new System.Drawing.Size(228, 26);
+            this.Search_INP.TabIndex = 5;
+            this.Search_INP.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Search_INP_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Search";
+            // 
+            // Refresh_BTN
+            // 
+            this.Refresh_BTN.Location = new System.Drawing.Point(700, 96);
+            this.Refresh_BTN.Name = "Refresh_BTN";
+            this.Refresh_BTN.Size = new System.Drawing.Size(72, 30);
+            this.Refresh_BTN.TabIndex = 7;
+            this.Refresh_BTN.Text = "Refresh";
+            this.Refresh_BTN.UseVisualStyleBackColor = true;
+            this.Refresh_BTN.Click += new System.EventHandler(this.Refresh_BTN_Click);
+            // 
             // MyContactsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(784, 545);
+            this.Controls.Add(this.Refresh_BTN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Search_INP);
+            this.Controls.Add(this.EditContact_BTN);
+            this.Controls.Add(this.RemoveContact_BTN);
+            this.Controls.Add(this.AddContact_BTN);
             this.Controls.Add(this.Contacts_GV);
             this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -116,18 +193,25 @@ namespace MyContacts
             this.Load += new System.EventHandler(this.MyContactsForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Contacts_GV)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView Contacts_GV;
+        private System.Windows.Forms.Button AddContact_BTN;
+        private System.Windows.Forms.Button Refresh_BTN;
+        private System.Windows.Forms.Button RemoveContact_BTN;
+        private System.Windows.Forms.Button EditContact_BTN;
         private System.Windows.Forms.DataGridViewTextBoxColumn ContactId;
         private System.Windows.Forms.DataGridViewTextBoxColumn ContactName;
         private System.Windows.Forms.DataGridViewTextBoxColumn ContactFamily;
         private System.Windows.Forms.DataGridViewTextBoxColumn ContactPhone;
         private System.Windows.Forms.DataGridViewTextBoxColumn ContactEmail;
         private System.Windows.Forms.DataGridViewTextBoxColumn ContactAddress;
+        private System.Windows.Forms.TextBox Search_INP;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,10 +19,83 @@ namespace MyContacts
             contactsOperations = new MyContactOperations();
         }
 
-        private void MyContactsForm_Load(object sender, EventArgs e)
+        private void RefreshContacts_GV()
         {
             Contacts_GV.AutoGenerateColumns = false;
             Contacts_GV.DataSource = contactsOperations.SelectAll();
+        }
+
+        private void MyContactsForm_Load(object sender, EventArgs e)
+        {
+            RefreshContacts_GV();
+        }
+
+        private void Refresh_BTN_Click(object sender, EventArgs e)
+        {
+            RefreshContacts_GV();
+        }
+
+        private void AddContact_BTN_Click(object sender, EventArgs e)
+        {
+            AddAndEditContact_FORM form = new AddAndEditContact_FORM();
+            form.Text = "Add contact";
+            form.ShowDialog();
+
+            if (form.DialogResult == DialogResult.OK)
+            {
+                RefreshContacts_GV();
+            }
+        }
+
+        private void Search_INP_KeyUp(object sender, KeyEventArgs e)
+        {
+            Contacts_GV.DataSource = contactsOperations.Search(Search_INP.Text);
+        }
+
+        private void EditContact_BTN_Click(object sender, EventArgs e)
+        {
+            if (Contacts_GV.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(Contacts_GV.CurrentRow.Cells[0].Value);
+
+                AddAndEditContact_FORM form = new AddAndEditContact_FORM();
+                form.UpdatingContactId = id;
+                form.Text = "Edit contact";
+                form.ShowDialog();
+
+                if (form.DialogResult == DialogResult.OK)
+                {
+                    RefreshContacts_GV();
+                }
+            }
+        }
+
+        private void RemoveContact_BTN_Click(object sender, EventArgs e)
+        {
+            if (Contacts_GV.CurrentRow != null)
+            {
+                string name = Convert.ToString(Contacts_GV.CurrentRow.Cells[1].Value);
+                string family = Convert.ToString(Contacts_GV.CurrentRow.Cells[2].Value);
+
+                if (MessageBox.Show($"Are you sure you want to delete {name} {family}?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int id = Convert.ToInt32(Contacts_GV.CurrentRow.Cells[0].Value);
+                    bool isContactDeleted = contactsOperations.Delete(id);
+                    if (isContactDeleted)
+                    {
+                        MessageBox.Show("The contact was deleted successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        RefreshContacts_GV();
+                    }
+                    else
+                    {
+                        MessageBox.Show("The operation failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
