28da76c709aacdd2d843543b784bae03bc26dcc7
amin pasban <amin.pasban.talesh@gmail.com>
2021-09-30 22:42:05 +0330

    "select", "search", "insert", "edit", "delete" methods added to project.

--- Services/MyContactOperations.cs
+++ Services/MyContactOperations.cs
@@ -11,7 +11,17 @@ namespace MyContacts
     class MyContactOperations : IMyContactsOperations
     {
         private string connectionString = "Data Source= .; Initial Catalog= MyContacts_DB; Integrated Security= true";
-        
+        public DataTable Select(int id)
+        {
+            string query = $"select * from MyContacts where id = {id}";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+        }
+
         public DataTable SelectAll()
         {
             string query = "select * from MyContacts";
@@ -23,24 +33,98 @@ namespace MyContacts
             return data;
         }
 
-        public DataTable Select(int id)
+        public DataTable Search(string searchedString)
         {
-            throw new NotImplementedException();
+            string query = "select * from MyContacts where name like @searchedString or family like @searchedString";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.SelectCommand.Parameters.AddWithValue("@searchedString", "%" + searchedString + "%");
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
         }
 
         public bool Insert(string name, string family, string phone, string email, string address)
         {
-            throw new NotImplementedException();
+            string query = "insert into MyContacts (Name, Family, Phone, Email, Address) values (@Name, @Family, @Phone, @Email, @Address)";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Phone", phone);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Address", address);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         public bool Update(int id, string name, string family, string phone, string email, string address)
         {
-            throw new NotImplementedException();
+            string query = "update MyContacts set Name = @Name, Family = @Family, Phone = @Phone, Email = @Email, Address = @Address where Id = @Id";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Phone", phone);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Address", address);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            string query = "delete from MyContacts where Id = @Id";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
